91085b46758b94e3f5dc77f08e290fe306d67e81
mustafa <mustafamjaber@yahoo.com>
2021-05-28 19:09:45 +0200

    added a contant to the combonet

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
-function Greeting() {
+// css
+import './indx.css';
+function BookList() {
 	return (
-		<div>
-			<h1>heloo mj</h1>
-		</div>
+		<section className="book">
+			<Book />
+		</section>
 	);
 }
+const Book = () => {
+	return (
+		<article className="books">
+			<Image />
+			<Title />
+			<Author />
+		</article>
+	);
+};
+const Image = () => {
+	return <img src="https://images-na.ssl-images-amazon.com/images/I/71XQe19mZtL._AC_UL200_SR200,200_.jpg" alt="" />;
+};
+const Title = () => {
+	const tit = 'My First Learn to Write Workbook: Practice';
+	return <h1>{tit}</h1>;
+};
+const Author = () => <h4 style={{ color: '#617d98', fontSize: '0.75rem', margin: '0.25rem' }}>Crystal Radke</h4>;
 
-ReactDom.render(<Greeting />, document.getElementById('root'));
+ReactDom.render(<BookList />, document.getElementById('root'));
